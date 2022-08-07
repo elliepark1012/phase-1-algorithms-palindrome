31e37037578eac64f663a4934e46562cc8a2a5eb
Ellie Park <elliepark1012@gmail.com>
2022-08-07 08:56:17 -0400

    commit tree c5ee6181e329512c205c8b0492c7f928dd93e978

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  myArray = word.split("") 
+  for (let i = 0; i < myArray.length / 2; i++) {
+    if (myArray[i] = myArray[myArray.length - i]) {
+    } 
+  }
+  return true
 }
+
 
 /* 
   Add your pseudocode here
-*/
+/ getting string letters one by one by array
+/ find out the array.length 
+/ check if array[0] = last array length is same letter   
+/ +1 -1 
 
 /*
   Add written explanation of your solution here
